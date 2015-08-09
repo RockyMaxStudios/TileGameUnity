b8c0f02b94aa9e3496f392e791bccf54899a8b9b
Zoombiniton <purplepacman@gmail.com>
2015-08-09 20:42:59 +0100

    Changed GenerateGridScripts

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class GenerateGridAP : MonoBehaviour {
 
-	private int noRows = 5;
-	private int noCols = 5;
+	private int noRows = 10;
+	private int noCols = 10;
+	private int screenWidth;
+	private int screenHeight;
 
 	// Use this for initialization
 	void Start () {
+
+		var sceneCamera = GameObject.Find ("Main Camera");
+		screenWidth = sceneCamera.GetComponent<Camera> ().pixelWidth;
+		screenHeight = sceneCamera.GetComponent<Camera> ().pixelHeight;
+
+		float squareSize = 40;
 
 		for (int i = 0; i< noRows; i++) {
 			for (int j = 0; j< noCols; j++) {
@@ -15,10 +23,11 @@ public class GenerateGridAP : MonoBehaviour {
 				square.transform.parent = GameObject.Find ("GamePanel").transform;
 				square.AddComponent<CanvasRenderer> ();
 				square.AddComponent<UnityEngine.UI.Image> ();
-	
-				square.transform.localScale = new Vector3 (0.8f, 0.8f, 1f);
-				square.transform.localPosition = new Vector3 (85 * i - 200, 85 * j - 200, 0f);
-			//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
+
+				square.transform.localScale = new Vector3 ((float)(squareSize/100f), (float)(squareSize/100f), 1f);
+				square.transform.localPosition = new Vector3 ((squareSize + 5) * i - ((noRows/2f) * (squareSize + 5)) + (squareSize/2f), (squareSize + 5) * j - ((noCols/2f) * (squareSize + 5)) + (squareSize/2f), 0f);
+
+				//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
 			}
 		}
 	}
