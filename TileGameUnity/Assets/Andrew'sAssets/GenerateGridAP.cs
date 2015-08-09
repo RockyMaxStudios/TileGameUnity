196fad400c38b32aa9887aeb35f2bc594d05d7e5
Zoombiniton <purplepacman@gmail.com>
2015-08-09 19:36:32 +0100

    Updated Generate Grid

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class GenerateGridAP : MonoBehaviour {
 
+	private int noRows = 5;
+	private int noCols = 5;
+
 	// Use this for initialization
 	void Start () {
-		GameObject square = new GameObject ("square", typeof(Plane));
-		//GameObject square = new Rect (100, 100, 100, 100);
-		//square.transform.localScale = new Vector3(0f, 0f, 0.00f);
-		//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
-		square.transform.parent = GameObject.Find("GamePanel").transform;
-		square.AddComponent<CanvasRenderer> ();
-		square.AddComponent<UnityEngine.UI.Image> ();
-		//GameObject tile = new GameObject ("tile", CanvasRenderer, Image, Panel);
+
+		for (int i = 0; i< noRows; i++) {
+			for (int j = 0; j< noCols; j++) {
+				GameObject square = new GameObject ("square-" + i + "-" + j, typeof(Plane));
+				square.transform.parent = GameObject.Find ("GamePanel").transform;
+				square.AddComponent<CanvasRenderer> ();
+				square.AddComponent<UnityEngine.UI.Image> ();
+	
+				square.transform.localScale = new Vector3 (0.8f, 0.8f, 1f);
+				square.transform.localPosition = new Vector3 (85 * i - 200, 85 * j - 200, 0f);
+			//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
+			}
+		}
 	}
 	
 	// Update is called once per frame
