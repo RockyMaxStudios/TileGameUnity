4bcdee3a67b682f7a081095807ca05d64eb6e4b5
Zoombiniton <purplepacman@gmail.com>
2015-08-09 22:15:49 +0100

    ClickEventStuff

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GenerateGridAP : MonoBehaviour {
 
@@ -7,6 +8,8 @@ public class GenerateGridAP : MonoBehaviour {
 	private int noCols = 10;
 	private int screenWidth;
 	private int screenHeight;
+	[HideInInspector] public List<Transform> planes = new List<Transform>();
+	Camera cameraComponent;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,8 @@ public class GenerateGridAP : MonoBehaviour {
 		var sceneCamera = GameObject.Find ("Main Camera");
 		screenWidth = sceneCamera.GetComponent<Camera> ().pixelWidth;
 		screenHeight = sceneCamera.GetComponent<Camera> ().pixelHeight;
+
+		cameraComponent = GameObject.Find("Main Camera").GetComponent<Camera>();
 
 		float squareSize = 47;
 
@@ -28,6 +33,8 @@ public class GenerateGridAP : MonoBehaviour {
 				square.transform.localScale = new Vector3 ((float)(squareSize/100f), (float)(squareSize/100f), 1f);
 				square.transform.localPosition = new Vector3 ((squareSize + 5) * i - ((noRows/2f) * (squareSize + 5)) + (squareSize/2f), (squareSize + 5) * j - ((noCols/2f) * (squareSize + 5)) + (squareSize/2f), 0f);
 
+
+				planes.Add(square.transform);
 				//square.GetComponent<Panel>().onClick.AddListener(() => { someFunction(); otherFunction(); }); 
 				//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
 			}
@@ -37,12 +44,42 @@ public class GenerateGridAP : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (Physics.Raycast(clickRay, 10))
+			Ray clickRay = cameraComponent.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHitInfo;
+			
+			if (Physics.Raycast(clickRay, out rayHitInfo))
 			{
-				print("Hit something");
+				print ("Hit Something");
+				foreach (Transform plane in planes)
+				{
+					if (rayHitInfo.collider.gameObject.transform == plane)
+					{
+						plane.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+					}
+				}
 			}
 		}
 	}
+
+	/*void FixedUpdate()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			
+			Ray clickRay = cameraComponent.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHitInfo;
+			
+			if (Physics.Raycast(clickRay, out rayHitInfo))
+			{
+				print ("Hit Something");
+				foreach (Transform plane in planes)
+				{
+					if (rayHitInfo.collider.gameObject.transform == plane)
+					{
+						plane.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+					}
+				}
+			}
+		}
+	}*/
 }
