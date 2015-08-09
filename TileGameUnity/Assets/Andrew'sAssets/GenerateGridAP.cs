3c5fbe15a3716a16d8707ee1030ba1b88922a2b5
Zoombiniton <purplepacman@gmail.com>
2015-08-09 18:24:03 +0100

    Drawing Tiles in Scripts

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -7,9 +7,12 @@ public class GenerateGridAP : MonoBehaviour {
 	void Start () {
 		GameObject square = new GameObject ("square", typeof(Plane));
 		//GameObject square = new Rect (100, 100, 100, 100);
-		square.transform.localScale = new Vector3(0.1f, 0.1f, 0.00f);
+		//square.transform.localScale = new Vector3(0f, 0f, 0.00f);
 		//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
 		square.transform.parent = GameObject.Find("GamePanel").transform;
+		square.AddComponent<CanvasRenderer> ();
+		square.AddComponent<UnityEngine.UI.Image> ();
+		//GameObject tile = new GameObject ("tile", CanvasRenderer, Image, Panel);
 	}
 	
 	// Update is called once per frame
