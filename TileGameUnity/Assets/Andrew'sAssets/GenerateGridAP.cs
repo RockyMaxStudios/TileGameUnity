42ebe8317a8d7d6d55817541c9a5eba0ff6ca999
Zoombiniton <purplepacman@gmail.com>
2015-08-09 21:29:00 +0100

    RayCasting

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -15,18 +15,20 @@ public class GenerateGridAP : MonoBehaviour {
 		screenWidth = sceneCamera.GetComponent<Camera> ().pixelWidth;
 		screenHeight = sceneCamera.GetComponent<Camera> ().pixelHeight;
 
-		float squareSize = 40;
+		float squareSize = 47;
 
 		for (int i = 0; i< noRows; i++) {
 			for (int j = 0; j< noCols; j++) {
 				GameObject square = new GameObject ("square-" + i + "-" + j, typeof(Plane));
 				square.transform.parent = GameObject.Find ("GamePanel").transform;
-				square.AddComponent<CanvasRenderer> ();
+				square.AddComponent<MeshRenderer> ();
+				square.AddComponent<MeshCollider> ();
 				square.AddComponent<UnityEngine.UI.Image> ();
 
 				square.transform.localScale = new Vector3 ((float)(squareSize/100f), (float)(squareSize/100f), 1f);
 				square.transform.localPosition = new Vector3 ((squareSize + 5) * i - ((noRows/2f) * (squareSize + 5)) + (squareSize/2f), (squareSize + 5) * j - ((noCols/2f) * (squareSize + 5)) + (squareSize/2f), 0f);
 
+				//square.GetComponent<Panel>().onClick.AddListener(() => { someFunction(); otherFunction(); }); 
 				//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
 			}
 		}
@@ -34,6 +36,13 @@ public class GenerateGridAP : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(clickRay, 10))
+			{
+				print("Hit something");
+			}
+		}
 	}
 }
