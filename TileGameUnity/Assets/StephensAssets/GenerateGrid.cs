8a2638161fa3c9fb449d30b2ff2bda5dfe033b1d
Stephen Lingham <pentex_1@msn.com>
2015-08-09 21:35:57 +0100

    Got the on click working so the user can click on panels of the grid

--- TileGameUnity/Assets/StephensAssets/GenerateGrid.cs
+++ TileGameUnity/Assets/StephensAssets/GenerateGrid.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GenerateGrid : MonoBehaviour
 {
     public Transform sceneCamera;
+    public Transform planePrefab;
     private Camera cameraComponent;
     public int numberOfHorizontalRectangles = 5;
     public int numberOfVerticalRectangles = 5;
     public float spacingBetweenRectangles = 0.05f;
     private float screenWidthInWorldUnits;
-    private float screenHeightInWorldUnits;    
+    private float screenHeightInWorldUnits;
+    [HideInInspector] public List<Transform> planes = new List<Transform>();
 
     void Start()
     {
@@ -21,18 +24,22 @@ public class GenerateGrid : MonoBehaviour
         for (int i = 0; i < numberOfHorizontalRectangles; i++)
             for (int j = 0; j < numberOfVerticalRectangles; j++)
             {
-                GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-                plane.transform.rotation = Quaternion.Euler(-90, 0, 0);
+                Transform plane = GameObject.Instantiate(planePrefab);
+                plane.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+
+                plane.rotation = Quaternion.Euler(-90, 0, 0);
 
                 float horizontalSpacing = cameraComponent.pixelWidth / numberOfHorizontalRectangles;
                 float verticalSpacing = cameraComponent.pixelHeight / numberOfVerticalRectangles;
-                plane.transform.position = cameraComponent.ScreenToWorldPoint(new Vector3(horizontalSpacing * 0.5f + horizontalSpacing * i, 
+                plane.position = cameraComponent.ScreenToWorldPoint(new Vector3(horizontalSpacing * 0.5f + horizontalSpacing * i, 
                                                                                           verticalSpacing * 0.5f + verticalSpacing * j, 
                                                                                           1));
 
-                plane.transform.localScale = new Vector3(1 * cameraComponent.aspect / numberOfHorizontalRectangles - spacingBetweenRectangles/2,
+                plane.localScale = new Vector3(1 * cameraComponent.aspect / numberOfHorizontalRectangles - spacingBetweenRectangles/2,
                                                          1, 
                                                          1f / numberOfVerticalRectangles - spacingBetweenRectangles/2);
+
+                planes.Add(plane);
             }
     }
 }
---
+++ TileGameUnity/Assets/StephensAssets/MemoryGame.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryGame : MonoBehaviour
+{
+    GenerateGrid generateGrid;
+    Camera cameraComponent;
+
+    void Start()
+    {
+        generateGrid = GameObject.Find("GenerateGrid").GetComponent<GenerateGrid>();
+        cameraComponent = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray clickRay = cameraComponent.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHitInfo;
+            
+            if (Physics.Raycast(clickRay, out rayHitInfo))
+            {
+                foreach (Transform plane in generateGrid.planes)
+                {
+                    if (rayHitInfo.collider.gameObject.transform == plane)
+                    {
+                        plane.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+                    }
+                }
+            }
+        }
+    }
+}
+
