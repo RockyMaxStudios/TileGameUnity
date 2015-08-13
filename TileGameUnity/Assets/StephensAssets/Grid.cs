df97e49ed69133d58ccfe60893de46c9583acffe
Stephen Lingham <pentex_1@msn.com>
2015-08-13 23:20:03 +0100

    Added click animation

--- TileGameUnity/Assets/StephensAssets/Grid.cs
+++ TileGameUnity/Assets/StephensAssets/Grid.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GenerateGrid : MonoBehaviour
+public class Grid : MonoBehaviour
 {
     public Transform sceneCamera;
     public Transform planePrefab;
@@ -12,9 +12,15 @@ public class GenerateGrid : MonoBehaviour
     public float spacingBetweenRectangles = 0.05f;
     private float screenWidthInWorldUnits;
     private float screenHeightInWorldUnits;
-    [HideInInspector] public List<Transform> planes = new List<Transform>();
+    [HideInInspector]
+    public List<Transform> planes = new List<Transform>();
 
     void Start()
+    {
+        GeneratePlanes();
+    }
+
+    private void GeneratePlanes()
     {
         cameraComponent = sceneCamera.GetComponent<Camera>();
 
@@ -27,19 +33,23 @@ public class GenerateGrid : MonoBehaviour
                 Transform plane = GameObject.Instantiate(planePrefab);
                 plane.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
 
+                //Set rotation
                 plane.rotation = Quaternion.Euler(-90, 0, 0);
 
+                //Set position
                 float horizontalSpacing = cameraComponent.pixelWidth / numberOfHorizontalRectangles;
                 float verticalSpacing = cameraComponent.pixelHeight / numberOfVerticalRectangles;
-                plane.position = cameraComponent.ScreenToWorldPoint(new Vector3(horizontalSpacing * 0.5f + horizontalSpacing * i, 
-                                                                                          verticalSpacing * 0.5f + verticalSpacing * j, 
+                plane.position = cameraComponent.ScreenToWorldPoint(new Vector3(horizontalSpacing * 0.5f + horizontalSpacing * i,
+                                                                                          verticalSpacing * 0.5f + verticalSpacing * j,
                                                                                           1));
 
-                plane.localScale = new Vector3(1 * cameraComponent.aspect / numberOfHorizontalRectangles - spacingBetweenRectangles/2,
-                                                         1, 
-                                                         1f / numberOfVerticalRectangles - spacingBetweenRectangles/2);
+                //Set scale
+                plane.localScale = new Vector3(1 * cameraComponent.aspect / numberOfHorizontalRectangles - spacingBetweenRectangles / 2,
+                                                         1,
+                                                         1f / numberOfVerticalRectangles - spacingBetweenRectangles / 2);
 
                 planes.Add(plane);
             }
     }
 }
+
--- TileGameUnity/Assets/StephensAssets/MemoryGame.cs
+++ TileGameUnity/Assets/StephensAssets/MemoryGame.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MemoryGame : MonoBehaviour
 {
-    GenerateGrid generateGrid;
+    Grid grid;
     Camera cameraComponent;
 
+    int sequenceLength = 3;
+    List<Transform> generatedSequence = new List<Transform>();
+    
     void Start()
     {
-        generateGrid = GameObject.Find("GenerateGrid").GetComponent<GenerateGrid>();
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
         cameraComponent = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     void Update()
     {
+        PlaySequence();
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Ray clickRay = cameraComponent.ScreenPointToRay(Input.mousePosition);
@@ -21,15 +27,29 @@ public class MemoryGame : MonoBehaviour
             
             if (Physics.Raycast(clickRay, out rayHitInfo))
             {
-                foreach (Transform plane in generateGrid.planes)
+                foreach (Transform plane in grid.planes)
                 {
                     if (rayHitInfo.collider.gameObject.transform == plane)
                     {
-                        plane.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+                        //plane.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+                        plane.GetComponent<PlaneScript>().Click();
                     }
                 }
             }
         }
+    }
+
+    private void PlaySequence()
+    {
+
+    }
+
+    private void GenerateSequence()
+    {
+        generatedSequence.Clear();
+
+        for (int i = 0; i < sequenceLength; i++)
+            generatedSequence.Add(grid.planes[Random.Range(0, grid.planes.Count)]);
     }
 }
 
