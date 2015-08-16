0552c7a77d5c1ab70bd8d8f0cee93c9bb39fb9c9
Stephen Lingham <pentex_1@msn.com>
2015-08-16 01:05:20 +0100

    Changes to Andrew scene grid

--- TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
+++ TileGameUnity/Assets/Andrew'sAssets/GenerateGridAP.cs
@@ -24,20 +24,33 @@ public class GenerateGridAP : MonoBehaviour {
 
 		for (int i = 0; i< noRows; i++) {
 			for (int j = 0; j< noCols; j++) {
-				GameObject square = new GameObject ("square-" + i + "-" + j, typeof(Plane));
-				square.transform.parent = GameObject.Find ("GamePanel").transform;
-				square.AddComponent<MeshFilter> ();
-				square.AddComponent<MeshRenderer> ();
-				square.AddComponent<MeshCollider> ();
-				square.AddComponent<UnityEngine.UI.Image> ();
+                //GameObject square = new GameObject ("square-" + i + "-" + j, typeof(Plane));
 
-				square.transform.localScale = new Vector3 ((float)(squareSize/100f), (float)(squareSize/100f), 1f);
-				square.transform.localPosition = new Vector3 ((squareSize + 5) * i - ((noRows/2f) * (squareSize + 5)) + (squareSize/2f), (squareSize + 5) * j - ((noCols/2f) * (squareSize + 5)) + (squareSize/2f), 0f);
+                GameObject square = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                square.gameObject.name = "square-" + i + "-" + j;
+                square.transform.parent = GameObject.Find("GamePanel").transform;
+                //square.AddComponent<MeshFilter> ();
+                //square.AddComponent<MeshRenderer> ();
+                //square.AddComponent<MeshCollider> ();
+				//square.AddComponent<UnityEngine.UI.Image> ();
+
+			
+                //square.transform.localPosition = new Vector3 ((squareSize + 5) * i - ((noRows/2f) * (squareSize + 5)) + (squareSize/2f), (squareSize + 5) * j - ((noCols/2f) * (squareSize + 5)) + (squareSize/2f), 0f);
+                var unityHeight = cameraComponent.orthographicSize * 2;
+                var unityWidth = (cameraComponent.orthographicSize * 2) * cameraComponent.aspect;
+                var scalingFactor = screenHeight/unityHeight;
+                var newSquareSize = squareSize/scalingFactor;
+
+                square.transform.localScale = new Vector3((float)(newSquareSize / 10f), 1f, (float)(newSquareSize / 10f));
+
+                square.transform.localPosition = new Vector3((newSquareSize * 1.01f) * i - ((noRows / 2f) * (newSquareSize * 1.01f)) + (newSquareSize / 2f), (newSquareSize * 1.01f) * j - ((noCols / 2f) * (newSquareSize * 1.01f)) + (newSquareSize / 2f), 0f);
+                
+                
 
 
 				planes.Add(square.transform);
 				//square.GetComponent<Panel>().onClick.AddListener(() => { someFunction(); otherFunction(); }); 
-				//square.transform.rotation =  new Quaternion(1.5f, 1f, 1f, 0f);
+                square.transform.rotation = Quaternion.Euler(new Vector3(270, 0, 0));
 			}
 		}
 	}
@@ -51,7 +64,7 @@ public class GenerateGridAP : MonoBehaviour {
 			
 			if (Physics.Raycast(clickRay, out rayHitInfo))
 			{
-				print ("Hit Something");
+				print ("Hit Something - " + rayHitInfo.collider.transform.gameObject.name);
 				foreach (Transform plane in planes)
 				{
 					if (rayHitInfo.collider.gameObject.transform == plane)
